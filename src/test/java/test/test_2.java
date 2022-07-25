01b989707219b06b91f624e67bea7352ecb83a17
unknown <narwadevinod25@gmail.com>
2022-07-25 17:18:50 +0530

    modified test2

--- src/test/java/test/test_2.java
+++ src/test/java/test/test_2.java
@@ -5,6 +5,8 @@ public class test_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("tst 2");
+         
+         System.out.println("modified");
 	}
 
 }
