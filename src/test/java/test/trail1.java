48e9408f3db4bb9851f1ea1e8bb51b9ea4dde1c8
unknown <narwadevinod25@gmail.com>
2022-07-25 16:42:27 +0530

    First Project

---
+++ src/test/java/test/trail1.java
@@ -0,0 +1,12 @@
+package test;
+
+public class trail1 {
+
+	public static void main(String[] args) {
+             int i=5;
+             for(int a=1;a<=10;a++) {
+            	System.out.println(i*a); 
+             }
+	}
+
+}
